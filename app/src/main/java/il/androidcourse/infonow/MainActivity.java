2608eb607b162fde767b19e0a14202d75069e3bd
Savva Spiridonov <savvasapir@gmail.com>
2024-05-18 14:56:36 +0300

    Android Studio Project Initialized

---
+++ app/src/main/java/il/androidcourse/infonow/MainActivity.java
@@ -0,0 +1,14 @@
+package il.androidcourse.infonow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
