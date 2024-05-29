2f1c4b493a25f2d75dad177d61df09c1afdab910
Savva Spiridonov <savvasapir@gmail.com>
2024-05-30 01:31:24 +0300

    feat: add features to news dialog

--- app/src/main/java/il/androidcourse/infonow/RSSItemAdapter.java
+++ app/src/main/java/il/androidcourse/infonow/RSSItemAdapter.java
@@ -1,11 +1,16 @@
 package il.androidcourse.infonow;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -20,6 +25,7 @@ public class RSSItemAdapter extends RecyclerView.Adapter<RSSItemAdapter.ViewHold
 
     private List<RSSItem> rssItems = new ArrayList<>();
     private int itemSize;
+    private Dialog dialog;
 
     public RSSItemAdapter(int itemSize) {
         this.itemSize = itemSize;
@@ -42,6 +48,39 @@ public class RSSItemAdapter extends RecyclerView.Adapter<RSSItemAdapter.ViewHold
         RSSItem item = rssItems.get(position);
         holder.titleView.setText(item.getTitle());
         holder.descriptionView.setText(item.getDescription());
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = v.getContext();
+                dialog = new Dialog(context);
+                dialog.setContentView(R.layout.item_rss_dialog_box);
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT); // 340 x 680
+                dialog.setCancelable(true);
+
+                ((TextView)dialog.findViewById(R.id.dialogTitleView)).setText(item.getTitle());
+                ((TextView)dialog.findViewById(R.id.dialogDescriptionView)).setText(item.getDescription());
+                dialog.findViewById(R.id.dialogButtonContinue).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent httpIntent = new Intent(Intent.ACTION_VIEW);
+                        httpIntent.setData(Uri.parse(item.getLink()));
+
+                        context.startActivity(httpIntent);
+                    }
+
+                });
+                dialog.findViewById(R.id.dialogButtonClose).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+
+                });
+                new ImageLoadTask(item.getImage(), ((ImageView)dialog.findViewById(R.id.dialogImg))).execute();
+                dialog.show();
+            }
+
+        });
         new ImageLoadTask(item.getImage(), holder.imageView).execute();
     }
 
